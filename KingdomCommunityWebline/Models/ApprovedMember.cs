d030d6d971ce1daf0c45c1b1225ae1d24dc61966
staticcoder96 <classiestephy67@yahoo.com>
2026-06-23 20:53:40 -0500

    Included the Login and Register buttons to the Landing Page.

--- KingdomCommunityWebline/Models/ApprovedMember.cs
+++ KingdomCommunityWebline/Models/ApprovedMember.cs
@@ -8,16 +8,17 @@ namespace KingdomCommunityWebline.Models
         public int ApprovedMemberId { get; set; }
 
         [Required]
-        public string FirstName { get; set; } 
+        public string FirstName { get; set; } = string.Empty;
 
         [Required]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
 
         [Required]
         [EmailAddress]
-        public string EmailAddress { get; set; }
+        public string EmailAddress { get; set; } = string.Empty;
 
-        public string PhoneNumber { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; } = string.Empty;
 
         public bool IsRegistered { get; set; } = false;
     }
